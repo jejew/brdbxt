bbc20aa182885a599be8018b55090eaf9802960d
jejew <40838667+jejew@users.noreply.github.com>
2019-05-31 00:15:49 +0200

    Update newton_method2.cpp

--- newton_method2.cpp
+++ newton_method2.cpp
@@ -2,24 +2,26 @@
 #include <cmath>
 using namespace std;
 
-template < typename E, typename F, typename T > T sqrt_newton(E e, F f, T x0, T num)
-{
-  const double tol = 0.000000005;
-  T new_guess;
 
-  new_guess = f((e(x0)/x0));
-  while (fabs((new_guess-x0)/new_guess) > tol)
+template < typename E, typename F, typename T> T sqrt_newton(E e, F f, T x0)
+{
+    double x1;
+  T new_guess;
+    
+    for (int i; i < x0; ++i)
     {
-       x0 = new_guess;
-       new_guess = f((x0+num/x0));
-     }
-  return new_guess;
+        x1 = (x0 - ((e(x0)/f(x0))));
+        x0 = x1;
+    }
+  
+  return x1;
+  
 }
 
 int main()
 {
   std::cout.precision(16);
-  cout << sqrt_newton([](double x){ return x + 612.0; }, [](double x){ return x/2.0; }, 10.0, 612.0);
+  cout << sqrt_newton([](double x){ return x * x - 612.0; }, [](double x){ return 2.0 * x; }, 10.0);
 
   return 0;
 }
