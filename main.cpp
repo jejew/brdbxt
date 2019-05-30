6db0aeb0afec4bf4912d30e0d543ab52de9f8c56
jejew <40838667+jejew@users.noreply.github.com>
2019-05-31 01:00:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,16 +13,26 @@ std::array<double, 2> fit (const std::vector<double>& x, const std::vector<doubl
     const auto n    = x.size();
     const auto s_x  = std::accumulate(x.begin(), x.end(), 0.0);
     const auto s_y  = std::accumulate(y.begin(), y.end(), 0.0);
-    const auto s_xx = std::inner_product(x.begin(), x.end(), x.begin(), 0.0);
-    const auto s_xy = std::inner_product(x.begin(), x.end(), y.begin(), 0.0);
-    const auto a    = (n * s_xy - s_x * s_y) / (n * s_xx - s_x * s_x);
-    const auto b    = (s_y - a*s_x);
+    const auto ms_y = s_y/n;
+    const auto ms_x = s_x/n;
+    double numerator = 0.0;
+    double denominator = 0.0;
+
+    for(int i=0; i<n; ++i){
+        numerator += (x[i] - ms_x) * (y[i] - ms_y);
+        denominator += (x[i] - ms_x) * (x[i] - ms_x);
+    }
+    const auto a    = numerator / denominator;
+    const auto b    = (ms_y - a*ms_x);
     std::array<double, 2> A = {a, b};
     return A;
 }
 
 int main() {
-    std::vector<double> x{6, 5, 11, 7, 5, 4, 4};
-    std::vector<double> y{2, 3, 9, 1, 8, 7, 5};
-    std::cout << fit(x,y);
+    std::vector<double> x{6, 5, 11, 7};
+    std::vector<double> y{2, 3, 9, 1};
+    std::array<double, 2> f = fit(x, y);
+    cout << f[0] << " , " << f[1] << endl;
+    cout << "a = 1.16867, b = -4.72289";
+    
 }
